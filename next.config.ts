f392c8dbe79b2f3414aec56ad66da814359f2e10
Minhal Rizvi <rizvi4600779@cloud.neduet.edu.pk>
2026-03-14 00:00:39 +0500

    updated backend with travel data

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   turbopack: {
     root: path.resolve(__dirname),
   },
